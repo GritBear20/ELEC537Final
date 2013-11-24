09b5870c08ead39af30fb323c50cec981113f8d6
Lee Xiong <lx8@rice.edu>
2013-11-24 17:54:08 -0600

    add repo

---
+++ repos/ns-3-allinone/ns-3-dev/build/build-status.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+# Programs that are runnable.
+ns3_runnable_programs = ['build/scratch/ns3-dev-tcp-Q3-debug', 'build/scratch/ns3-dev-scratch-simulator-debug', 'build/scratch/ns3-dev-sim_1-debug', 'build/scratch/ns3-dev-tcp-Q1-debug', 'build/scratch/subdir/ns3-dev-subdir-debug', 'build/scratch/ns3-dev-tcp-weiqi_q1-debug', 'build/scratch/ns3-dev-tcp-variants-comparison_q2-debug', 'build/scratch/ns3-dev-sim_2-debug']
+
+# Scripts that are runnable.
+ns3_runnable_scripts = []
+
---
+++ repos/wifi-hiddent-terminal-simulation1.cc
@@ -0,0 +1,149 @@
+  /*
+  * Classical hidden terminal problem and its RTS/CTS solution.
+  *
+  * Topology: [node 0] <-- -50 dB --> [node 1] <-- -50 dB --> [node 2]
+  *
+  * This example illustrates the use of
+  * - Wifi in ad-hoc mode
+  * - Matrix propagation loss model
+  * - Use of OnOffApplication to generate CBR stream
+  * - IP flow monitor
+  */
+#include "ns3/core-module.h"
+#include "ns3/propagation-module.h"
+#include "ns3/network-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/flow-monitor-module.h"
+#include "ns3/wifi-module.h"
+ 
+using namespace ns3;
+ 
+void experiment (bool enableCtsRts)
+{
+  // 0. Enable or disable CTS/RTS
+  UintegerValue ctsThr = (enableCtsRts ? UintegerValue (100) : UintegerValue (2200));
+  Config::SetDefault ("ns3::WifiRemoteStationManager::RtsCtsThreshold", ctsThr);
+ 
+  // 1. Create 3 nodes
+  NodeContainer nodes;
+  nodes.Create (3);
+ 
+  // 2. Place nodes somehow, this is required by every wireless simulation
+  for (size_t i = 0; i < 3; ++i)
+  {
+  nodes.Get (i)->AggregateObject (CreateObject<ConstantPositionMobilityModel> ());
+  }
+ 
+  // 3. Create propagation loss matrix
+  Ptr<MatrixPropagationLossModel> lossModel = CreateObject<MatrixPropagationLossModel> ();
+  lossModel->SetDefaultLoss (200); // set default loss to 200 dB (no link)
+  lossModel->SetLoss (nodes.Get (0)->GetObject<MobilityModel>(), nodes.Get (1)->GetObject<MobilityModel>(), 50); // set symmetric loss 0 <-> 1 to 50 dB
+  lossModel->SetLoss (nodes.Get (2)->GetObject<MobilityModel>(), nodes.Get (1)->GetObject<MobilityModel>(), 50); // set symmetric loss 2 <-> 1 to 50 dB
+ 
+  // 4. Create & setup wifi channel
+  Ptr<YansWifiChannel> wifiChannel = CreateObject <YansWifiChannel> ();
+  wifiChannel->SetPropagationLossModel (lossModel);
+  wifiChannel->SetPropagationDelayModel (CreateObject <ConstantSpeedPropagationDelayModel> ());
+
+  // 5. Install wireless devices
+  WifiHelper wifi;
+  wifi.SetStandard (WIFI_PHY_STANDARD_80211b);
+  wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
+  "DataMode",StringValue ("DsssRate2Mbps"),
+  "ControlMode",StringValue ("DsssRate1Mbps"));
+  YansWifiPhyHelper wifiPhy = YansWifiPhyHelper::Default ();
+  wifiPhy.SetChannel (wifiChannel);
+  NqosWifiMacHelper wifiMac = NqosWifiMacHelper::Default ();
+  wifiMac.SetType ("ns3::AdhocWifiMac"); // use ad-hoc MAC
+  NetDeviceContainer devices = wifi.Install (wifiPhy, wifiMac, nodes);
+ 
+  // uncomment the following to have athstats output
+  // AthstatsHelper athstats;
+  // athstats.EnableAthstats(enableCtsRts ? "basic-athstats-node" : "rtscts-athstats-node", nodes);
+ 
+  // uncomment the following to have pcap output
+  //wifiPhy.EnablePcap (enableCtsRts ? "basic-pcap-node" : "rtscts-pcap-node", nodes);
+ 
+ 
+  // 6. Install TCP/IP stack & assign IP addresses
+  InternetStackHelper internet;
+  internet.Install (nodes);
+  Ipv4AddressHelper ipv4;
+  ipv4.SetBase ("10.0.0.0", "255.0.0.0");
+  ipv4.Assign (devices);
+ 
+  // 7. Install applications: two CBR streams each saturating the channel
+  ApplicationContainer cbrApps;
+  uint16_t cbrPort = 12345;
+  OnOffHelper onOffHelper ("ns3::UdpSocketFactory", InetSocketAddress (Ipv4Address ("10.0.0.2"), cbrPort));
+  onOffHelper.SetAttribute ("PacketSize", UintegerValue (200));
+  onOffHelper.SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1]"));
+  onOffHelper.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0]"));
+ 
+  // flow 1: node 0 -> node 1
+  onOffHelper.SetAttribute ("DataRate", StringValue ("3000000bps"));
+  onOffHelper.SetAttribute ("StartTime", TimeValue (Seconds (1.000000)));
+  cbrApps.Add (onOffHelper.Install (nodes.Get (0)));
+
+  // flow 2: node 2 -> node 1
+  onOffHelper.SetAttribute ("DataRate", StringValue ("3001100bps"));
+  onOffHelper.SetAttribute ("StartTime", TimeValue (Seconds (1.001)));
+  cbrApps.Add (onOffHelper.Install (nodes.Get (2)));
+ 
+  uint16_t echoPort = 9;
+  UdpEchoClientHelper echoClientHelper (Ipv4Address ("10.0.0.2"), echoPort);
+  echoClientHelper.SetAttribute ("MaxPackets", UintegerValue (1));
+  echoClientHelper.SetAttribute ("Interval", TimeValue (Seconds (0.1)));
+  echoClientHelper.SetAttribute ("PacketSize", UintegerValue (10));
+  ApplicationContainer pingApps;
+ 
+  // again using different start times to workaround Bug 388 and Bug 912
+  echoClientHelper.SetAttribute ("StartTime", TimeValue (Seconds (0.001)));
+  pingApps.Add (echoClientHelper.Install (nodes.Get (0)));
+  echoClientHelper.SetAttribute ("StartTime", TimeValue (Seconds (0.006)));
+  pingApps.Add (echoClientHelper.Install (nodes.Get (2)));
+ 
+ 
+ 
+ 
+  // 8. Install FlowMonitor on all nodes
+  FlowMonitorHelper flowmon;
+  Ptr<FlowMonitor> monitor = flowmon.InstallAll ();
+ 
+  // 9. Run simulation for 10 seconds
+  Simulator::Stop (Seconds (10));
+  Simulator::Run ();
+ 
+  // 10. Print per flow statistics
+  monitor->CheckForLostPackets ();
+  Ptr<Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier> (flowmon.GetClassifier ());
+  std::map<FlowId, FlowMonitor::FlowStats> stats = monitor->GetFlowStats ();
+  for (std::map<FlowId, FlowMonitor::FlowStats>::const_iterator i = stats.begin (); i != stats.end (); ++i)
+  {
+  // first 2 FlowIds are for ECHO apps, we don't want to display them
+  if (i->first > 2)
+  {
+  Ipv4FlowClassifier::FiveTuple t = classifier->FindFlow (i->first);
+  std::cout << "Flow " << i->first - 2 << " (" << t.sourceAddress << " -> " << t.destinationAddress << ")\n";
+  std::cout << " Tx Bytes: " << i->second.txBytes << "\n";
+  std::cout << " Rx Bytes: " << i->second.rxBytes << "\n";
+  std::cout << " Throughput: " << i->second.rxBytes * 8.0 / 10.0 / 1000 / 1000 << " Mbps\n";
+  }
+  }
+ 
+  // 11. Cleanup
+  Simulator::Destroy ();
+ }
+ 
+ int main (int argc, char **argv)
+ {
+  std::cout << "Hidden station experiment with RTS/CTS disabled:\n" << std::flush;
+  experiment (false);
+  std::cout << "------------------------------------------------\n";
+  std::cout << "Hidden station experiment with RTS/CTS enabled:\n";
+  experiment (true);
+ 
+  return 0;
+ }
