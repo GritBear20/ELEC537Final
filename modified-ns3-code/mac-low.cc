a2fb36902738aee5d339532d704d46944dc52169
gritbear20 <lx8@rice.edu>
2013-12-04 14:16:44 -0600

    start to change CTS handling

--- modified-ns3-code/mac-low.cc
+++ modified-ns3-code/mac-low.cc
@@ -747,7 +747,13 @@ if (hdr.IsCts ()){
                 //should set its cnt down timer to priority
                 
           //}
+        if (hdr.GetAddr1 () != m_self){
+
+             Ptr<ns3::Timer> tdelay = new Timer();
+             tdelay->Schedule(hdr.GetDuration());
+        }
       }
+        
 }
 
 if (hdr.IsData() && usingLLTBasedAlgo){
